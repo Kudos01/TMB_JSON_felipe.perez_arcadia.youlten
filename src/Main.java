205d32900e86515a7e579504047f7224a05bbb50
rsmnr <arcadia.youlten@students.salle.url.edu>
2019-12-16 17:14:11 +0100

    Basic tutorial of stuff shown by alfonso

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
+//package com.company;
 
-            //hewwow
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+// APP ID AND APP KEY
+// app_id=41936f32&app_key=3c5639afc8280c17cb4f633b78de717b
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        //https://www.vogella.com/tutorials/JavaLibrary-OkHttp/article.html
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                //change url -- needs to return json
+                .url(  "https://api.tmb.cat/v1/transit/parades?app_id=41936f32&app_key=3c5639afc8280c17cb4f633b78de717b")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println( response.body().string());
+
     }
 }
