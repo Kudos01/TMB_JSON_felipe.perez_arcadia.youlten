bf896cbd1b0a58ef3ba7f7d649604055c21ccabf
rsmnr <arcadia.youlten@students.salle.url.edu>
2019-12-17 23:09:39 +0100

    updated menu class with correct menu information - has not been tested

--- src/Menu.java
+++ src/Menu.java
@@ -2,22 +2,27 @@ import java.util.Scanner;
 
 public class Menu {
 
-    private static final int MIN = 1;
+    private static final int MIN1 = 1;
+    private static final char MIN2 = 'a';
     private static final int MAX1 = 5;
-    private static final int MAX2 = 4;
+    private static final char MAX2 = 'f';
 
-    private static final String option1menu2 = "1: Sort teams by winrate";
-    private static final String option2menu2 = "2: Sort players by nationality";
-    private static final String option3menu2 = "3: Sort players by their KDA and winrate";
+    private static final String option1menu1 = "1. User Management";
+    private static final String option2menu1 = "2. Search Location";
+    private static final String option3menu1 = "3. Plan a route";
+    private static final String option4menu1 = "4. Bus wait time";
 
-    private static final String method1menu1 = "1: Sort using Quicksort";
-    private static final String method2menu1 = "2: Sort using Mergesort";
-    private static final String method3menu1 = "3: Sort using Bucketsort";
-    private static final String method4menu1 = "4: Sort using Radixsort";
+    private static final String optionamenu2 = "a) My Locations";
+    private static final String optionbmenu2 = "b) Location History";
+    private static final String optioncmenu2 = "c) My Routes";
+    private static final String optiondmenu2 = "d) Favorite stops and stations";
+    private static final String optionemenu2 = "e) Stations inaugurated by my birth year";
 
-    private static final String exitmenu2 = "4: Go back";
+
+    private static final String exitmenu2 = "f) Back to the principal menu";
     private static final String exitmenu1 = "5: Exit";
     private static final String ask = "Select an option: ";
+
 
     private int option;
     private Scanner scanner;
@@ -30,10 +35,11 @@ public class Menu {
     //prints all of the information related to menu2
     public void printMenu2() {
         System.out.println("");
-        System.out.println(option1menu2);
-        System.out.println(option2menu2);
-        System.out.println(option3menu2);
-        System.out.println(exitmenu2);
+        System.out.println(option1menu1);
+        System.out.println(option2menu1);
+        System.out.println(option3menu1);
+        System.out.println(option4menu1);
+        System.out.println(exitmenu1);
         System.out.println("");
 
     }
@@ -41,11 +47,12 @@ public class Menu {
     //prints all of the info related to menu1
     public void printMenu1() {
         System.out.println("");
-        System.out.println(method1menu1);
-        System.out.println(method2menu1);
-        System.out.println(method3menu1);
-        System.out.println(method4menu1);
-        System.out.println(exitmenu1);
+        System.out.println(optionamenu2);
+        System.out.println(optionbmenu2);
+        System.out.println(optioncmenu2);
+        System.out.println(optiondmenu2);
+        System.out.println(optionemenu2);
+        System.out.println(exitmenu2);
         System.out.println("");
     }
 
@@ -58,10 +65,11 @@ public class Menu {
     }
 
     public boolean validOption1() {
-        return option >= MIN && option <= MAX1;
+        return option >= MIN1 && option <= MAX1;
     }
     public boolean validOption2() {
-        return option >= MIN && option <= MAX2;
+
+        return option >= MIN2 && option <= MAX2;
     }
 
     public int getOption() {
