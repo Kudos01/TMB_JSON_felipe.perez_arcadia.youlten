622ae9ee8af5c6e518bee43c902d5341112c7db2
rsmnr <arcadia.youlten@students.salle.url.edu>
2019-12-24 11:11:57 +0100

    added fav location to the pack.

---
+++ src/DataModel/FavLocation.java
@@ -0,0 +1,21 @@
+package DataModel;
+
+import java.sql.Date;
+
+public class FavLocation {
+
+    private String type;
+    private Date date;
+
+    public String getType() { return type; }
+
+    public Date getDate() { return date; }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
