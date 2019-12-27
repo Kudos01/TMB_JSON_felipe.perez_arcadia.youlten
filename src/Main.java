b371369180bef899d18979f5f617990be6b4c581
rsmnr <arcadia.youlten@students.salle.url.edu>
2019-12-27 21:38:05 +0100

    Json fully parsed?

--- src/Main.java
+++ src/Main.java
@@ -63,85 +63,16 @@ public class Main {
             ArrayList<Hotel> hotels = new ArrayList<>();
             ArrayList<Restaurant> restaurants = new ArrayList<>();
             ArrayList<Monument> monuments = new ArrayList<>();
-            //Type tempListType = new TypeToken<ArrayList<temp>>(){}.getType();
+            Type tempListType = new TypeToken<ArrayList<temp>>(){}.getType();
             //temp temps = new temp();
             ArrayList<temp> temps = new ArrayList<>();
 
             //get the json object
             locOb = gson.fromJson(reader, LocationObj.class);
             //if you run locOb.getLocations() you get the json list of locations
+            temps = gson.fromJson(locOb.getLocations(),tempListType);
 
-            //get the json objects of the json array
-            for (int i = 0; i < locOb.getLocations().size(); i++) {
-                temp sub= new temp();
-                sub.setName(locOb.getLocations().get(i).);
-              temps.add(sub);
-
-            }
-
-            System.out.println(temps);
-
-            //System.out.println(generics);
-
-
-            //the idea of this part is once you get here, there should be an arraylist of generics (see temp)
-            for(int i=0; i<; i++){
-
-                Hotel tempH = new Hotel();
-                Monument tempM = new Monument();
-                Restaurant tempR = new Restaurant();
-                Place tempP = new Place();
-
-                if(temps.get(i).getStars() != null){
-
-                    tempH.setName(temps.get(i).getName());
-                    tempH.setCoordinates(temps.get(i).getCoordinates());
-                    tempH.setDescription(temps.get(i).getDescription());
-                    tempH.setStars(temps.get(i).getStars());
-
-                    hotels.add(tempH);
-                    System.out.println(hotels);
-                }
-                else if(temps.get(i).getArchitect() != null){
-                    /*
-
-                    tempM.setName(generics.get(i).getName());
-                    tempM.setCoordinates(generics.get(i).getCoordinates());
-                    tempM.setDescription(generics.get(i).getDescription());
-
-                    monuments.add(tempM);
-
-                     */
-
-                }
-                else if(temps.get(i).getCharacteristics() != null){
-
-                    //if characteristics is not null
-                    /*
-                    tempR.setName(temps.get(i).getName());
-                    tempR.setCoordinates(temps.get(i).getCoordinates());
-                    tempR.setDescription(temps.get(i).getDescription());
-
-                     restaurants.add(tempR);
-
-                     */
-
-                }
-                else{
-                    //add to location
-                    /*
-                    tempP.setName(temps.get(i).getName());
-                    tempP.setCoordinates(temps.get(i).getCoordinates());
-                    tempP.setDescription(temps.get(i).getDescription());
-
-                    locations.add(tempP);
-                     */
-
-                }
-
-
-
-            }
+            //System.out.println(temps);
 
 
 
